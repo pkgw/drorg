41e65f93b010b67fc24dd7d54962edd13f69cc2b
Peter Williams <peter@newton.cx>
2018-11-14 15:29:02 -0500

    Get multi-account login working.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,94 @@
 
 //! The main CLI driver logic.
 
+extern crate app_dirs;
 #[macro_use] extern crate failure;
 extern crate google_drive3;
-#[macro_use] extern crate structopt;
+extern crate hyper;
+extern crate hyper_native_tls;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+extern crate structopt;
+extern crate tempfile;
+extern crate yup_oauth2;
 
-use failure::{Error, Fail};
+use failure::Error;
+use std::fs;
 use std::process;
 use structopt::StructOpt;
+use yup_oauth2::{
+    Authenticator, ApplicationSecret, ConsoleApplicationSecret,
+    DefaultAuthenticatorDelegate, FlowType, GetToken, NullStorage,
+};
+
+mod token_storage;
 
 
-#[derive(Debug, StructOpt)]
-pub struct DriverCloseOptions {
-    #[structopt(help = "A thingie")]
-    what: String,
+/// Information used to find out app-specific config files, e.g. the
+/// application secret.
+const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo { name: "goodriver", author: "PKGW" };
+
+
+/// Get the "application secret" needed to authenticate against Google APIs.
+///
+/// TODO: can we automate the creation and retrieval of this file? That would
+/// be cool but not something to spend time on right now.
+///
+/// On Linux the desired filepath is `~/.config/goodriver/client_id.json`.
+fn get_app_secret() -> Result<ApplicationSecret, Error> {
+    let p = app_dirs::get_app_dir(app_dirs::AppDataType::UserConfig, &APP_INFO, "client_id.json")?;
+    let f = fs::File::open(p)?;
+    let cfg: ConsoleApplicationSecret = serde_json::from_reader(f)?;
+    cfg.installed.ok_or_else(|| format_err!("no installed-application secret"))
 }
 
-impl DriverCloseOptions {
+/// Get an HTTP client with all the bells and whistles we need.
+fn get_http_client() -> Result<hyper::Client, Error> {
+    Ok(hyper::Client::with_connector(
+        hyper::net::HttpsConnector::new(
+            hyper_native_tls::NativeTlsClient::new()?
+        )
+    ))
+}
+
+
+/// The command-line action to add a login to the credentials DB.
+#[derive(Debug, StructOpt)]
+pub struct DriverLoginOptions {
+    #[structopt(help = "An email address associated with the account")]
+    email: String,
+}
+
+impl DriverLoginOptions {
+    /// The auth flow here will print out a message on the console, asking the
+    /// user to go to a URL, following instructions, and paste a string back
+    /// into the client.
+    ///
+    /// We want to allow the user to login to multiple accounts
+    /// simultaneously. Therefore we set up the authenticator flow with a null
+    /// storage, and then add the resulting token to the disk storage.
     fn cli(self) -> Result<i32, Error> {
+        let scopes = token_storage::get_scopes();
+        let mut multi_storage = token_storage::get_storage()?;
+
+        let mut auth = Authenticator::new(
+            &get_app_secret()?,
+            DefaultAuthenticatorDelegate,
+            get_http_client()?,
+            NullStorage::default(),
+            Some(FlowType::InstalledInteractive)
+        );
+
+        let token = match auth.token(scopes.as_vec()) {
+            Ok(t) => t,
+
+            // Can't figure out how to adopt `e` into a failure::Error here:
+            Err(e) => return Err(format_err!("auth failed: {}", e))
+        };
+
+        multi_storage.add_token(&scopes, &self.email, token)?;
+        multi_storage.save_to_json()?;
         Ok(0)
     }
 }
@@ -28,15 +99,15 @@ impl DriverCloseOptions {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "driver", about = "Deal with Google Drive.")]
 pub enum DriverCli {
-    #[structopt(name = "close")]
-    /// Close a document or something
-    Close(DriverCloseOptions),
+    #[structopt(name = "login")]
+    /// Add a Google account to be monitored
+    Login(DriverLoginOptions),
 }
 
 impl DriverCli {
     fn cli(self) -> Result<i32, Error> {
         match self {
-            DriverCli::Close(opts) => opts.cli(),
+            DriverCli::Login(opts) => opts.cli(),
         }
     }
 }
---
+++ src/token_storage.rs
@@ -0,0 +1,173 @@
+// Copyright 2018 Peter Williams <peter@newton.cx>
+// Licensed under the MIT License.
+
+//! Storage of API tokens for multiple Google accounts simultaneously.
+
+use failure::Error;
+use serde_json;
+use std::collections::HashMap;
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
+use yup_oauth2::{Token, TokenStorage};
+
+
+/// Get a backend for storing authentication tokens.
+///
+/// This uses app_dirs and is specific to this application.
+pub fn get_storage() -> Result<DiskMultiTokenStorage, Error> {
+    let p = app_dirs::get_app_dir(app_dirs::AppDataType::UserConfig, &::APP_INFO, "tokens.json")?;
+    DiskMultiTokenStorage::new(p)
+}
+
+/// Get a ScopeList representing the scopes that we need.
+///
+/// This list is specific to this application.
+pub fn get_scopes() -> ScopeList<'static> {
+    // This string is `google_drive3::Scope::Full.as_ref()`. I can't get
+    // lifetimes to work out in this context.
+    ScopeList::new(&["https://www.googleapis.com/auth/drive"])
+}
+
+
+/// A helper type for yup_oauth2 scope lists, which are hashed
+/// in a specific way.
+pub struct ScopeList<'a> {
+    scopes: Vec<&'a str>,
+    hash: u64
+}
+
+impl<'a> ScopeList<'a> {
+    /// Create a new ScopeList using the specified list of scope URLs.
+    pub fn new<I, T>(scopes: I) -> ScopeList<'a>
+        where T: AsRef<str> + Ord + 'a,
+              I: IntoIterator<Item = &'a T>
+    {
+        // This copy-pastes the logic from yup_oauth2.
+        use std::collections::hash_map::DefaultHasher;
+        use std::hash::{Hash, Hasher};
+
+        let mut sv: Vec<&str> = scopes.into_iter()
+            .map(|s| s.as_ref())
+            .collect::<Vec<&str>>();
+        sv.sort();
+        let mut sh = DefaultHasher::new();
+        &sv.hash(&mut sh);
+        let sv = sv;
+
+        ScopeList { scopes: sv, hash: sh.finish() }
+    }
+
+    /// Get a reference to the list of scope URLs.
+    ///
+    /// This is suitable for passing to `yup_oauth2::GetToken::token()` and
+    /// `yup_oauth2::TokenStorage::set()`.
+    pub fn as_vec(&self) -> &Vec<&'a str> {
+        &self.scopes
+    }
+}
+
+
+/// This is just yup_oauth2::MemoryStorage, but implementing Serialize and
+/// Deserialize. And Debug.
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct SerdeMemoryStorage {
+    pub tokens: HashMap<u64, Token>,
+}
+
+impl TokenStorage for SerdeMemoryStorage {
+    // This type must implement std::error::Error, which failure::Error
+    // actually doesn't. So for convenience we use io::Error.
+    type Error = io::Error;
+
+    fn set(&mut self, scope_hash: u64, _: &Vec<&str>, token: Option<Token>) -> Result<(), io::Error> {
+        match token {
+            Some(t) => self.tokens.insert(scope_hash, t),
+            None => self.tokens.remove(&scope_hash),
+        };
+        Ok(())
+    }
+
+    fn get(&self, scope_hash: u64, _: &Vec<&str>) -> Result<Option<Token>, io::Error> {
+        match self.tokens.get(&scope_hash) {
+            Some(t) => Ok(Some(t.clone())),
+            None => Ok(None),
+        }
+    }
+}
+
+
+/// A way to serialize multiple sets of OAuth tokens all at once. This is
+/// basically yup_oauth2::DiskTokenStorage, but with an extra layer of hashmap
+/// at the top.
+///
+/// If you add new tokens to the storage, you must remember to write back out
+/// the updated file with `save_to_json()`.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DiskMultiTokenStorage {
+    path: PathBuf,
+    accounts: HashMap<String, SerdeMemoryStorage>,
+}
+
+impl DiskMultiTokenStorage {
+    /// Create a new storage linked to the specified path.
+    ///
+    /// Data in the storage are loaded up creation. If the specified file does
+    /// not exist, the error is swallowed.
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<DiskMultiTokenStorage, Error> {
+        let mut dmts = DiskMultiTokenStorage {
+            path: path.as_ref().to_owned(),
+            accounts: HashMap::new(),
+        };
+
+        if let Err(e) = dmts.load_from_json() {
+            if e.kind() != io::ErrorKind::NotFound {
+                return Err(e.into());
+            }
+        }
+
+        Ok(dmts)
+    }
+
+    /// Fill in `self.accounts` with information gathered from the JSON-format
+    /// file `self.path`.
+    fn load_from_json(&mut self) -> Result<(), io::Error> {
+        let f = fs::File::open(&self.path)?;
+        self.accounts = serde_json::from_reader(f)?;
+        Ok(())
+    }
+
+    /// Write out the current stored token information to the backing file.
+    pub fn save_to_json(&self) -> Result<(), io::Error> {
+        let mut destdir = self.path.clone();
+        destdir.pop();
+
+        let temp = tempfile::Builder::new()
+            .prefix("tokens")
+            .suffix(".json")
+            .tempfile_in(destdir)?;
+
+        serde_json::to_writer(&temp, &self.accounts)?;
+
+        temp.persist(&self.path)?;
+        Ok(())
+    }
+
+    /// Get a mutable reference to the token storage associated with the given key.
+    ///
+    /// If the key was not present, a new empty token storage is created.
+    pub fn get_mut<S: AsRef<str>>(&mut self, key: S) -> &mut SerdeMemoryStorage {
+        // I don't like cloning the key by default, but I can never figure out
+        // how to avoid it without borrowck problems.
+        self.accounts.entry(key.as_ref().to_owned()).or_insert(SerdeMemoryStorage::default())
+    }
+
+    /// Insert a new token into the storage.
+    pub fn add_token<'a, S: AsRef<str>>(
+        &mut self, scopes: &ScopeList<'a>, key: S, token: Token
+    ) -> Result<(), Error>
+    {
+        let storage = self.get_mut(key);
+        Ok(storage.set(scopes.hash, &scopes.scopes, Some(token))?)
+    }
+}
